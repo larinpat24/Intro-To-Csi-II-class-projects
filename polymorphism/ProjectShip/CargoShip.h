7835d0f650104a90520667d688345b71c356f61d
Niral Patel <NiralP@student.fdu.edu>
2018-12-28 15:39:43 -0500

    Create CargoShip.h

---
+++ polymorphism/ProjectShip/CargoShip.h
@@ -0,0 +1,32 @@
+//Cargo ship header file 
+#pragma once
+#include <iostream>
+#include <string>
+# include "Ship.h"
+
+class CargoShip: public Ship
+{
+private:
+	int CargoCap;
+public:
+//constructors 
+	CargoShip(string n, string y, int c = 0):Ship(n, y)
+	{
+		CargoCap = c;
+	}
+//setters and getters 
+	void setCargoCap(int c)
+	{
+		CargoCap = c;
+	}
+	int getCargoCap()
+	{
+		return CargoCap;
+	}
+	//INSERT overriden VIRTUAL PRINT FUNCTION HERE TO DISPLAY SHIP NAME AND CARGO CAP
+	virtual void print() const 
+	{ 
+		cout<< "Ship cargo capacity is: " << CargoCap <<" and name is: " <<name<< endl;
+	}
+
+};
